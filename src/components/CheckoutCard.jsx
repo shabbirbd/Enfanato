b3596899fd843e776ef9ea7f6840466c37d0214d
shabbirbd <shabbirhossainshah@gmail.com>
2023-12-19 20:05:30 +0600

    adding component

--- src/components/CheckoutCard.jsx
+++ src/components/CheckoutCard.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { CartContext } from '../App';
+import { products } from '../data/products';
+
+
 
 const CheckoutCard = () => {
+    const [show, setShow] = useState(false);
+    const [cart, setCart] = useContext(CartContext);
+    const totalCartedItem = cart.reduce((sum, item)=> sum + item.quantity, 0);
+    const cartedProducts = cart.map((item)=>{
+        return products.find((product)=> item.productId === product.id )
+     });
     return (
         <div className='border border-neutral-300'>
-            checkout card
+            <div className={`duration-300 w-full ${show ? "h-auto" : "h-10"} px-2 border border-red-300 overflow-hidden`}>
+                <div className='flex items-center justify-between h-10 w-full text-textal text-sm'>
+                    <p>{totalCartedItem} {totalCartedItem > 1 ? "items" : "item"}</p>
+                    <button onClick={()=>setShow(!show)} className='flex items-center'>{show ? "Hide details" : "Show details"} {show ? <IoIosArrowUp /> : <IoIosArrowDown />}</button>
+                </div>
+                <div className='w-full h-[1px] bg-neutral-300 my-2'></div>
+                <div>
+                    hello
+                </div>
+
+            </div>
         </div>
     );
 };
